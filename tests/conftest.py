ca499cedd49256d32dfc9b32b605dba1eacd3add
panda <pandalinoox@gmail.com>
2026-06-02 20:47:22 +0530

    test: add LadderMode e2e tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,12 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+
 from tests.support.fixtures.battles import default_double_battle, empty_double_battle
-from tests.support.fixtures.players import offline_metronome_player
+from tests.support.fixtures.players import (
+    offline_metronome_player,
+)
 from tests.support.fixtures.tera_strategies import first_turn_tera_strategy
 
 __all__ = [
@@ -8,3 +15,16 @@ __all__ = [
     "offline_metronome_player",
     "first_turn_tera_strategy",
 ]
+
+load_dotenv()
+
+
+@pytest.fixture(scope="session")
+def credentials():
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+
+    assert username is not None, "USERNAME env var is required"
+    assert password is not None, "PASSWORD env var is required"
+
+    return username, password
--- tests/e2e/test_smoke_e2e.py
+++
@@ -1,2 +0,0 @@
-def test_smoke_e2e():
-    assert True
